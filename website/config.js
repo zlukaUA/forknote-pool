537bd6a01834cc55c79ee95c2916b43bc7517517
zlukaUA <36016635+zlukaUA@users.noreply.github.com>
2018-02-01 01:03:56 +0200

    Update config.js

--- website/config.js
+++ website/config.js
@@ -1,22 +1,22 @@
-var api = "http://poolhost.com:1117";
+var api = "http://yourpoolhost.com:8117";
 
-var api_blockexplorer = "http://daemonhost.com:1118";
+var api_blockexplorer = "http://yourpoolhost.com:32348";
 
-var poolHost = "poolhost.com";
+var poolHost = "yourpoolhost.com";
 
 var irc = "irc.freenode.net/#poolhost";
 
-var email = "support@poolhost.com";
+var email = "support@yourpoolhost.com";
 
-var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-UAH"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "http://explorer.karbowanec.com/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "http://explorer.karbowanec.com/?hash={id}#blockchain_transaction";
 
-var themeCss = "themes/default-theme.css";
+var themeCss = "themes/motherboard-dark-theme.css";
 
 var networkStat = {
     "bcn": [
@@ -42,14 +42,41 @@ var networkStat = {
         ["fcn.mypool.online", "http://fcn.mypool.online:24084"]
     ],
     "krb": [
-        ["krb.mypool.online", "http://krb.mypool.online:32351"],
-        ["democats.org", "http://pool2.democats.org:7673"],
+        ["krb.pool.ualinux.com", "http://krb.pool.ualinux.com:8117"],
+        ["krb.mypool.online", "http://api.krb.mypool.online:32351"],
+        ["democats.org/pool/?name=karbowanec", "http://pool2.democats.org:7673"],
         ["pool.karbowanec.com", "http://pool.karbowanec.com:8117"],
-        ["pool2.karbowanec.com", "http://pool2.karbowanec.com:8117"],
         ["krb.sberex.com", "http://krb.sberex.com:7006"],
         ["krb.crypto-coins.club", "http://krb.crypto-coins.club:8118"],
         ["krb.cryptonotepool.com", "http://5.189.135.137:8618"],
-        ["krbpool.ml", "http://krbpool.ml:8117"]
+        ["karbowanec.cryptopool.in", "http://karbowanec.cryptopool.in:8117"],
+        ["krb.cryptomine.pro", "http://krb.cryptomine.pro:8117"],
+        ["krb.kopanka.com", "http://krb.kopanka.com:8117"],
+        ["krb.mininglamp.ml", "http://krb.mininglamp.ml:8117"],
+        ["krb.multipool.online", "http://krb.multipool.online:8117"],
+        ["krb.tfoto.com.ua", "http://krb.tfoto.com.ua:8117"],
+        ["usa.krb.kopanka.com", "http://usa.krb.kopanka.com:8117"],
+        ["pool.krb-ua.tk", "http://pool.krb-ua.tk:8117"],
+        ["krb.i-holder.net", "http://krb.i-holder.net:8117"],
+        ["krbpool.ml", "http://krbpool.ml:8117"],
+        ["krb.fastpool.io", "http://krb.fastpool.io:8185"],
+        ["krb.ocukminingpool.com", "http://ocukminingpool.com:8118"],
+        ["pool2.karbowanec.com", "http://45.77.55.161:8117"],
+        ["krb.dreampool.info", "https://krb.dreampool.info/api"],
+        ["krb.miner.rocks", "https://krb.miner.rocks/api"],
+        ["easyhash.io/start/krb", "https://api-krb.easyhash.io:443"],
+        ["mine4all.pp.ua", "http://pool2.democats.org:7673"],
+        ["krb-pool.pp.ua", "http://krb-pool.pp.ua:25417"],
+        ["krb.mininggood.com", "http://us2.mininggood.com:8137"],
+        ["krbpool.com", "http://api.krbpool.com:8117"],
+        ["krb.soyminero.es", "http://krb.soyminero.es:8119"],
+        ["karbunkul.ga", "http://172.104.225.140:3389"],
+        ["karbo.coinwire.eu", "http://karbo.coinwire.eu:8118"],
+        ["krb.miningpool.org.ua", "http://165.227.163.153:7118"],
+        ["karbowanec.hashparty.io", "http://karbowanec-pool.hashparty.io:8117"],
+
+        ["karbo.farm", "http://karbo.farm:8117"],
+        ["karbo.hashvault.pro", "http://karbo.hashvault.pro:8117"]
     ],
     "qcn": [
         ["qcn.mypool.online", "http://qcn.mypool.online:23084"]
